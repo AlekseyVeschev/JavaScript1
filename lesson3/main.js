867eddef8bf8920658473c39d0431b945baf766c
Aleksey <aleksey.veshev@mail.ru>
2020-12-25 18:27:16 +0300

    fixTree

---
+++ lesson3/main.js
@@ -0,0 +1,105 @@
+"use strict"
+//?Задание 1
+for (let i = 0; i <= 10; i++) {
+   if (i === 0) {
+      console.log(i, "это ноль")
+   } else if (i % 2) {
+      console.log(i, "нечетное число")
+   } else {
+      console.log(i, "четное число")
+   }
+}
+//?Задание 2
+const post = {
+   author: "John",  //вывести этот текст 
+   postId: 23,
+   comments:
+      [
+         {
+            userId: 10,
+            userName: "Alex",
+            text: "lorem ipsum",
+            rating: {
+               likes: 10,
+               dislikes: 2  //вывести это число 
+            }
+         },
+         {
+            userId: 5,   //вывести это число 
+            userName: "Jane",
+            text: "lorem ipsum 2",  //вывести этот текст 
+            rating: {
+               likes: 3,
+               dislikes: 1
+            }
+         },
+      ]
+};
+// console.log("post.author :", post.author)
+// console.log("post[comments] :", post.comments[0].rating.dislikes)
+// console.log("post[comments] :", post.comments[1].userId)
+// console.log("post[comments] :", post.comments[1].text)
+
+//?Задание 3
+// const products =
+//    [
+//       {
+//          id: 3,
+//          price: 200,
+//       },
+//       {
+//          id: 4,
+//          price: 900,
+//       },
+//       {
+//          id: 1,
+//          price: 1000,
+//       },
+//    ];
+
+// products.forEach(product => {
+//    console.log(`"id:"  ${product.id}" price:" ${product.price / 100 * 85}`)
+// })
+
+//?Задание 4
+const products =
+   [
+      {
+         id: 3,
+         price: 127,
+         photos: [
+            "1.jpg",
+            "2.jpg",
+         ]
+      },
+      {
+         id: 5,
+         price: 499,
+         photos: []
+      },
+      {
+         id: 10,
+         price: 26,
+         photos: [
+            "3.jpg"
+         ]
+      },
+      {
+         id: 8,
+         price: 78,
+      },
+   ];
+
+const productsFilterSort = products
+   .filter(product => product.photos && product.photos.length)
+   .sort((a, b) => a.price - b.price)
+
+console.log(productsFilterSort)
+
+//?Задание 5
+// for (let i = 0; i < 9; i++, console.log(i)) { }
+
+//?Задание 6
+for (let i = 0, X = ""; i < 20; i++) {
+   console.log(X += "X")
+}
